6d685b45777114027d23d8efa02780841bac28c2
Priyanshu Awasthi <nppriyansh112@gmail.com>
2025-08-08 15:19:35 +0545

    added the users follow properties

--- bookmarks/account/models.py
+++ bookmarks/account/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.conf import settings
+from django.contrib.auth import get_user_model
 
 # Create your models here.
 
@@ -37,3 +38,15 @@ class Contact(models.Model):
         ordering = ['-created']
     def __str__(self):
         return f'{self.user_from} follows {self.user_to}'
+    
+
+user_model = get_user_model()
+user_model.add_to_class(
+    'following',
+    models.ManyToManyField(
+        'self',
+        through=Contact,
+        related_name='followers',
+        symmetrical=False,
+    ),
+)

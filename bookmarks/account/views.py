7eecd733b777820cb0dbec3d0114be0a02f8865b
Priyanshu Awasthi <nppriyansh112@gmail.com>
2025-08-08 15:36:17 +0545

    added actions , adding feed to your dashboard of your followers and people you may know

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -40,12 +40,21 @@ def user_login(request):
 @login_required
 def dashboard(request):
         actions = Action.objects.exclude(user=request.user)
+        following_ids = request.user.following.values_list(
+            'id', flat=True
+        )
+        if following_ids:
+            actions= actions.filter(user_id__in=following_ids)
+        actions = actions.select_related(
+            'user','user__profile'
+        ).prefetch_related('target')[:10]
         return render(
             request,
             'account/dashboard.html',
-            {'section':'dashboard'}
+            {'section':'dashboard',
+            'actions': actions
+            }
             )
-        
 
 def register(request):
     if request.method == 'POST':

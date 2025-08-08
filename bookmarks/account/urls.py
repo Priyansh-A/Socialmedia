ecf9f9f84a08bdf93e0fbeff065a453cdc61e059
Priyanshu Awasthi <nppriyansh112@gmail.com>
2025-08-08 15:48:26 +0545

    added follower counts and stuff

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -38,12 +38,12 @@ urlpatterns = [
     #     auth_views.PasswordResetCompleteView.as_view(),
     #     name='password_reset_complete'
     # ),
-        path('',include('django.contrib.auth.urls')),
-        path('',views.dashboard,name='dashboard'),
-        path('register/', views.register ,name='register'),
-        path('edit/',views.edit,name='edit'),
-        path('users/', views.user_list, name='user_list'),
-        path('users/<username>/',views.user_detail,name='user_detail'),
-        path('users/follow/', views.user_follow, name='user_follow'),
+    path('', include('django.contrib.auth.urls')),
+    path('', views.dashboard, name='dashboard'),
+    path('register/', views.register, name='register'),
+    path('edit/', views.edit, name='edit'),
+    path('users/', views.user_list, name='user_list'),
+    path('users/follow/', views.user_follow, name='user_follow'),
+    path('users/<username>/', views.user_detail, name='user_detail'),
     ]
 
